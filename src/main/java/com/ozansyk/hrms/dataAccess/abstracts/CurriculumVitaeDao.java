f426c4273e41ed911ecb50a6c4cecf2b441438fc
Ozan Soyak <ozan.soyak@anadoluefes.com>
2022-03-25 18:22:01 +0300

    fix and changes.

--- src/main/java/com/ozansyk/hrms/dataAccess/abstracts/CurriculumVitaeDao.java
+++ src/main/java/com/ozansyk/hrms/dataAccess/abstracts/CurriculumVitaeDao.java
@@ -10,10 +10,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CurriculumVitaeDao extends JpaRepository<CurriculumVitae, Integer> {
-	@Query("SELECT new com.ozansyk.hrms.entities.dtos.responseDtos.CurriculumVitaeDtoResponse" +
+	@Query(value = "SELECT new com.ozansyk.hrms.entities.dtos.responseDtos.CurriculumVitaeDtoResponse" +
 			"(c.id, c.githubAddress, c.linkedinAddress, c.educations, c.jobExperience, c.languages, c.photos, c.programingLanguages, c.coverLetters) " +
-			"FROM CurriculumVitae c WHERE c.jobSeeker =: jobSeeker")
+			"FROM CurriculumVitae c JOIN Education e ON c.id = e.curriculumVitae.id JOIN JobExperience j ON c.id = j.curriculumVitae.id " +
+			"JOIN Language l ON c.id = l.curriculumVitae.id JOIN Photo p ON c.id = p.curriculumVitae.id JOIN ProgramingLanguage p2 ON c.id = p2.curriculumVitae.id " +
+			"JOIN CoverLetter c2 ON c.id = c2.curriculumVitae.id WHERE c.jobSeeker =: jobSeeker", nativeQuery = true)
 	CurriculumVitaeDtoResponse getByJobSeeker2(JobSeeker jobSeeker);
 
 	CurriculumVitae getByJobSeeker(JobSeeker jobSeeker);
 }
+
